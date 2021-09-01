de4654a0642d3b74a8137222bf63bf3e783af3b8
Patrick Wu <Ptrckwu@yahoo.com>
2021-08-31 21:59:31 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+function Monday() {
+    exerciseRoutine(liftWeights)
+  }
+  function runFiveMiles() {
+      console.log("Go for a five mile run");
+  }
+  function liftWeights() {
+    console.log("Pump iron");
+  }
+  function swimFortyLaps() {
+    console.log("Swim 40 laps");
+  }
+  function exerciseRoutine(postRunActivity) {
+    runFiveMiles();
+    postRunActivity();
+  }
+*/
+
+function receivesAFunction(callback) {
+    callback()
+};
+function returnsANamedFunction() {
+   return function brandNew() {
+    x + y
+   }
+};
+function returnsAnAnonymousFunction() {
+    return (function () {})
+}
